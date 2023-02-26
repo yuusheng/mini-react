6e8e1e98bd818e5364b933d1ed9257ef90e657ae
yuusheng <hi@yuusheng.im>
2023-02-26 20:31:29 +0800

    feat: valid

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello React'
+  }
+}
+
+const container = document.querySelector('#root')!
+
+const node = document.createElement(element.type)
+node.title = element.props.title
+
+const textNode = document.createTextNode('')
+textNode.nodeValue = element.props.children
+
+node.appendChild(textNode)
+container.appendChild(node)
