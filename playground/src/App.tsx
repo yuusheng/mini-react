ff5b5c59ec039f5ace78130516101b9957873034
yuusheng <hi@yuusheng.im>
2024-03-10 19:28:59 +0800

    feat: support deletion

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'mini-react'
+
 let count = 1
+let fooValue = true
 
 function App() {
   return (
@@ -21,12 +23,25 @@ function Counter() {
     console.log('click')
   }
 
+  function toggleFooBar() {
+    React.update()
+    fooValue = !fooValue
+  }
+
+  const Foo = () => <div>foo</div>
+  const bar = <p>bar</p>
+
   return (
     <div>
-      Counter: {count}
-      <button onClick={handleClick}>click</button>
+      {/* Counter: {count}
+      <button onClick={handleClick}>click</button> */}
+
+      <button onClick={toggleFooBar}>toggle</button>
+      <span>
+        <div>{ fooValue ? <Foo /> : bar }</div>
+        <span>wooo</span>
+      </span>
     </div>
   )
 }
-
-export default App
+export default Counter
